ad434fe805a026294c171e1d743aaf04970d38cb
Pawel Jonczyk <pjonczyk07@gmail.com>
2022-01-20 17:43:17 +0100

    delete unused part of code

--- hand_tracking.py
+++ hand_tracking.py
@@ -5,11 +5,10 @@ import mediapipe as mp
 class handDetector():
 
     def __init__(self, mode=False, max_hands=2):
+        # static mode
         self.mode = mode
         self.max_hands = max_hands
-
         self.mp_hands = mp.solutions.hands
-        self.hands = self.mp_hands.Hands()
         self.hands = self.mp_hands.Hands(
             self.mode, self.max_hands)
         self.mp_draw = mp.solutions.drawing_utils
@@ -21,7 +20,7 @@ class handDetector():
         img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
         # get detection
         self.results = self.hands.process(img_rgb)
-        # if hands are detected
+        # if hands are detected - multi_hand_landmarks output of hands landmarks
         if self.results.multi_hand_landmarks:
             for hand_landmark in self.results.multi_hand_landmarks:
                 if draw:
@@ -38,7 +37,7 @@ class handDetector():
             my_hand = self.results.multi_hand_landmarks[hand_number]
             for id, landmark in enumerate(my_hand.landmark):
                 height, width, channel = img.shape
-                # make it form decimal to int
+                # make it form decimal to int(pixels)
                 pos_x, pos_y = int(
                     landmark.x * width), int(landmark.y * height)
                 landmark_list.append([id, pos_x, pos_y])
